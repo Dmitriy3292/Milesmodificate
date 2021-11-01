3e0d31d269839a177e629b6c7329f7826d33ca5f
Dmitriy3292 <mr.diy@yandex.ru>
2021-11-01 18:48:02 +0400

    final

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,9 +1,10 @@
 public class BonusMilesService {
-    public int calculate ( int price);{
-        int price = 9_000;
+    public int calculate(int price)
+
+    {
         int rub = 20;
 
         int miles = (price / rub);
+        return miles;
     }
-
 }
